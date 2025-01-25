b26b6e8bebb41e61829f866477b4c1cd977564a8
Leon <leonsmirna8@gmail.com>
2025-01-25 19:40:32 +0300

    Task 4

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-import random
+import sqlite3
 import sys
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QColor, QPainter
-from random import randint
-from UI import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.paint)
-        self.do_paint = False
-        self.circles = []
+        uic.loadUi('main.ui', self)
+        connection = sqlite3.connect("coffee.sqlite")
+        cur = connection.cursor()
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            for i in self.circles:
-                qp.setPen(i[0])
-                qp.drawEllipse(*i[1:])
-            qp.end()
-        self.do_paint = False
+        res = cur.execute('SELECT * FROM coffee').fetchall()
 
-    def paint(self):
-        self.do_paint = True
-        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
-        color = QColor(r, g, b)
-        r = random.randint(5, 250)
-        center_x = random.randint(0, 500 - r) + r // 2
-        center_y = random.randint(0, 500 - r) + r // 2
-        self.circles.append([color, center_x, center_y, r, r])
-        self.update()
+        titles = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах',
+                  'описание вкуса', 'цена (₽)', 'объем упаковки (кг)']
+        self.tableWidget.setColumnCount(len(res[0]))
+        self.tableWidget.setHorizontalHeaderLabels(titles)
+        self.tableWidget.setRowCount(len(res))
+
+        for i, row in enumerate(res):
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = DBSample()
     ex.show()
     sys.exit(app.exec())
